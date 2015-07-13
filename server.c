810b018b7e3bc3a3618cce68d658f97dedec55b0
Kevin Allison <kevin.allison.5@us.af.mil>
2015-07-13 13:31:45 -0400

    Modified server, start work on different ids

--- server.c
+++ server.c
@@ -16,29 +16,30 @@
 
 #include "ip.h"
 
-#define NO_FLAGS		0
 #define DEFAULT_PORT	9050
-#define DEFAULT_TIMEOUT	5
-#define WAIT_FOR_CONN	-1
+#define WAIT_FOR_CONN	0
+#define TEST_RUNNING	1
+
+#define FLAG_START		0x1
 
 struct pkt_buffer {
 	uint64_t id;
-	uint64_t total;
+	uint8_t flags;
+	int16_t timeout;
 };
 
 //void print_stats(struct pkt_buffer *pkt, uint64_t pkt_count)
-void print_stats(uint64_t last_pkt_id, uint64_t last_pkt_count, uint64_t pkt_count)
+void print_stats(uint64_t last_pkt_id, uint64_t pkt_count)
 {
-	double recv_rate = (pkt_count / (1.0 * last_pkt_count));
+	double recv_rate = (pkt_count / (1.0 * pkt_count));
 	printf("=================\n");
 	printf("Last Pkt Id:  %ld\n", last_pkt_id);
-	printf("Packets Sent: %ld\n", last_pkt_count);
 	printf("Packets Recv: %ld\n", pkt_count);
-	printf("Recv Rate: %f%%\n", recv_rate);
+	printf("Recv Rate: %f%%\n", recv_rate * 100);
 	printf("=================\n");
 }
 
-void run_server(short port, long timeout)
+void run_server(short port)
 {
 	int sd;
 	struct sockaddr_storage addr;
@@ -63,45 +64,53 @@ void run_server(short port, long timeout)
 	}
 
 	uint64_t last_pkt_id = 0;	
-	uint64_t last_pkt_count = 0;
-	int64_t last_pkt_wait = WAIT_FOR_CONN;
-	time_t last_pkt_recv;
+
+	clock_t start_clock;
+	clock_t last_pkt_clock;
+	float clock_timeout;
 
 	uint64_t pkt_count = 0;
 	struct pkt_buffer *pkt;
 	for (;;) {
-		if (recvfrom(sd, buffer, BUFFER_SIZE, MSG_DONTWAIT, (struct sockaddr*)&addr, &socklen) < 0) { 
-			// Check to see if we timed out 
-			if (errno == EAGAIN) {
-				// Are we waiting to start a connection?
-				if (last_pkt_wait == WAIT_FOR_CONN)
-					continue;
-				// Has our time for this test expired?
-				if (time(NULL) > (last_pkt_recv + last_pkt_wait))
-					goto reset_conn;
-				else
-					continue;
-			}
-			
+		if (recvfrom(sd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&addr, &socklen) < 0) { 
 			perror("recvfrom");
 			return;
 		}
 
 		pkt = (struct pkt_buffer*)buffer;
-		last_pkt_id = pkt->id;
-		last_pkt_count = pkt->total;
-		last_pkt_wait = DEFAULT_TIMEOUT;
-		last_pkt_recv = time(NULL);
-
-		if (++pkt_count < pkt->total)
+		if ((pkt->flags & FLAG_START) != FLAG_START)
 			continue;
+	
+		clock_timeout = (pkt->timeout * 1.0);
 
-reset_conn:
-		print_stats(last_pkt_id, last_pkt_count, pkt_count);
-		pkt_count = 0;
-		last_pkt_count = 0;
-		last_pkt_id = 0;
-		last_pkt_wait = WAIT_FOR_CONN;
+		start_clock = clock();
+		// Handle the connection we received
+		for (;;) {
+			if (recvfrom(sd, buffer, BUFFER_SIZE, MSG_DONTWAIT, (struct sockaddr*)&addr, &socklen) < 0) { 
+				// Check to see if we timed out 
+				if (errno == EAGAIN) {
+					// Has our time for this test expired?
+					float time_running = (1.0 * ((float)clock() - (float)start_clock)) / CLOCKS_PER_SEC;
+					if (time_running > clock_timeout) {
+						print_stats(last_pkt_id, pkt_count);
+
+						pkt_count = 0;
+						last_pkt_id = 0;
+						start_clock = 0;
+						last_pkt_clock = 0;
+
+						break;
+					}
+				}
+
+				continue;
+			}
+
+			pkt = (struct pkt_buffer*)buffer;
+			last_pkt_clock = clock();
+			last_pkt_id = pkt->id;
+			++pkt_count;
+		}
 	}
 
 	close(sd);
@@ -118,11 +127,10 @@ void print_help(char *progname)
 		   "A simple UDP server for testing packet throughput on a network\n\n"
 		   "Options:\n"
 		   "  -p	UDP port to use (default = 9050)\n"
-		   "  -t	Timeout in seconds (default = 10)\n"
 		   "  -h	Help (Prints this document)\n"
 		   "\nExamples:\n"
-		   "%s			A default server running on UDP port 9050 with a timeout of 10 seconds\n"
-		   "%s -p 80 -t 25		UDP server running on port 80 with a timeout of 25 seconds\n",
+		   "%s		A default server running on UDP port 9050\n"
+		   "%s -p 80 		UDP server running on port 80\n",
 		   progname, progname, progname);
 }
 
@@ -130,9 +138,8 @@ int main(int argc, char **argv)
 {
 	int opt;
 	short port = DEFAULT_PORT;			// UDP port number
-	long timeout = DEFAULT_TIMEOUT;		// In seconds
 
-	while ((opt = getopt(argc, argv, "hp:t:")) != -1) {
+	while ((opt = getopt(argc, argv, "hp:")) != -1) {
 		switch (opt) {
 		case 'h':
 			print_help(argv[0]);
@@ -140,16 +147,13 @@ int main(int argc, char **argv)
 		case 'p':
 			port = atoi(optarg);
 			break;
-		case 't':
-			timeout = atol(optarg);
-			break;	
 		default:
 			printf("Usage: %s\n", argv[0]);
 			exit(EXIT_FAILURE);
 		}
 	}
 
-	run_server(port, timeout);
+	run_server(port);
 
 	return 0;
 }
