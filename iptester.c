46096fcfc48865ca97cdd922c91528df4118a1bc
Kevin Allison <kevin.allison.5@us.af.mil>
2015-07-16 15:38:02 -0400

    Removed binary, updated .gitignore to block it.
    Also modifications to the iptester code.

--- iptester.c
+++ iptester.c
@@ -28,30 +28,61 @@
 #define FLAG_NONE			0x00
 #define FLAG_START			0x01
 
+#define	BASE_10				10			// For use in strtol
+
 struct pkt_buffer {
-	uint64_t id;						// Id of each packet sent (usually incremented)
+	uint32_t hash;						// Unique hash to this packet stream
+	uint32_t id;						// Id of each packet sent (usually incremented)
 	uint8_t	flags;						// Flags, 0x01 -> START
 	int16_t timeout;					// Timeout between each packet (in seconds)
+	uint16_t size;						// Size of the packet
 };
 
-//void print_stats(struct pkt_buffer *pkt, uint64_t pkt_count)
-void print_stats(uint64_t last_pkt_id, uint64_t pkt_count)
+static const char *SIZES[] = {"Bytes", "KB", "MB"};
+
+/**
+ * Print stats about the transfer, recv rate, # of packets recv, etc
+ *
+ * \param[in] last_pkt_id	Id of the last packet to arrive
+ * \param[in] pkt_count		Total number of packets to arrive
+ */
+void print_stats(uint32_t last_pkt_id, uint32_t pkt_count, int32_t pkt_size)
 {
-	double recv_rate = (pkt_count / (1.0 * pkt_count));
+	double recv_rate = (pkt_count / (1.0 * last_pkt_id));
+	int dropped_pkts = last_pkt_id - pkt_count;
+
+	int i = 0;
+	while ((i < 2) && (pkt_size > 1024)) {
+		pkt_size = pkt_size / 1024;
+		++i;	
+	}	
+
 	printf("=================\n");
-	printf("Last Pkt Id:  %ld\n", last_pkt_id);
-	printf("Packets Recv: %ld\n", pkt_count);
-	printf("Recv Rate: %f%%\n", recv_rate * 100);
+	printf("Pkt Size:     %u %s\n", pkt_size, SIZES[i]);
+	printf("Last Pkt Id:  %u\n", last_pkt_id);
+	printf("Packets Recv: %u\n", pkt_count);
+	printf("Recv Rate:    %f%%\n", recv_rate * 100);
+	printf("Dropped Pkts: %d\n", dropped_pkts);
 	printf("=================\n");
 }
 
-void run_client(char *ip, short port, short size, int16_t timeout)
+/**
+ * Run the client, send UDP packets to ip:port with size for timeout
+ *
+ * \param[in] ip		IP Address of destination
+ * \param[in] port		Port on destination IP
+ * \param[in] size		Size of packets to send
+ * \param[in] timeout	Duration to send packets
+ */
+void run_client(char *ip, int16_t port, int16_t size, int16_t timeout)
 {
 	int sd;
 	struct sockaddr_in servaddr;
-	char buffer[BUFFER_SIZE];
+	char buffer[size];
 	struct pkt_buffer *pkt = (struct pkt_buffer*)buffer;
+	FILE *urandom = fopen("/dev/urandom", "r");
 	pkt->timeout = timeout;
+	pkt->size = size;
 
 	sd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sd < 0) {
@@ -68,35 +99,45 @@ void run_client(char *ip, short port, short size, int16_t timeout)
 	clock_t stop_clock = start_clock + (timeout * CLOCKS_PER_SEC);
 
 	// Send initial packet
-	pkt->id = 0;
+	pkt->id = 0x00;
 	pkt->flags = FLAG_START;
-	sendto(sd, buffer, sizeof(struct pkt_buffer), 0, (struct sockaddr*)&servaddr, sizeof(servaddr));
+	// Read in rand hash
+	//fread(&pkt->hash, sizeof(pkt->hash), 1, urandom);
+	pkt->hash = 0xdeadbeef;
+	sendto(sd, pkt, sizeof(struct pkt_buffer), 0, (struct sockaddr*)&servaddr, sizeof(servaddr));
 
+	printf("Sent packet with info:\n\tHash: 0x%0x\n\tId: %u\n\tFlags: %d\n\tTimeout: %d\n", pkt->hash, pkt->id, pkt->flags, pkt->timeout);
+
+
+	if (sizeof(pkt) < size) {
+		// Fill in random info until we reach the appropriate size
+		int16_t padd_data_size = size - sizeof(struct pkt_buffer);
+		fread(buffer + sizeof(struct pkt_buffer), padd_data_size, 1, urandom);
+	}
+	
 	pkt->flags = FLAG_NONE;
 	int loop_count;
-	//for (loop_count = 0; loop_count < num_pkts; loop_count++) {
 	for (loop_count = 1; clock() < stop_clock; loop_count++) {
 		pkt->id = loop_count;
-		if (sizeof(pkt) < size) {
-			// Fill in random info until we reach the appropriate size
-			short padd_data_size = size - sizeof(struct pkt_buffer);
-			FILE *urandom = fopen("/dev/urandom", "r");
-			fread(buffer + sizeof(struct pkt_buffer), padd_data_size, 1, urandom);
-			fclose(urandom);
-		}
 
 		if (sendto(sd, buffer, size, 0, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
 			perror("sendto");
-			return;
+			break;
 		}	
 	}
 
 	printf("Sent %d Packets\n", loop_count);
 
+	fclose(urandom);
 	close(sd);
 }
 
-void run_server(short port)
+/**
+ * Run a server listening on port
+ *
+ * \param[in] port			Port to listen on
+ */
+void run_server(int16_t port)
 {
 	int sd;
 	struct sockaddr_storage addr;
@@ -120,14 +161,18 @@ void run_server(short port)
 		return;
 	}
 
-	uint64_t last_pkt_id = 0;	
+	uint16_t recv_pkt_size = 0;
+	uint32_t last_pkt_id = 0;	
+	int32_t last_pkt_size = 0;	
 
 	clock_t start_clock;
-	clock_t last_pkt_clock;
+	//clock_t last_pkt_clock;
 	float clock_timeout;
 
 	uint64_t pkt_count = 0;
 	struct pkt_buffer *pkt;
+
+	printf("Server listening on port %d\n", port);
 	for (;;) {
 		if (recvfrom(sd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&addr, &socklen) < 0) { 
 			perror("recvfrom");
@@ -139,22 +184,26 @@ void run_server(short port)
 			continue;
 	
 		clock_timeout = (pkt->timeout * 1.0);
+		uint32_t current_hash = pkt->hash;
+		char inner_buffer[pkt->size];
+		recv_pkt_size = pkt->size;
 
 		start_clock = clock();
 		// Handle the connection we received
 		for (;;) {
-			if (recvfrom(sd, buffer, BUFFER_SIZE, MSG_DONTWAIT, (struct sockaddr*)&addr, &socklen) < 0) { 
+			int32_t pkt_size = recvfrom(sd, inner_buffer, recv_pkt_size, MSG_DONTWAIT, (struct sockaddr*)&addr, &socklen);
+			if (pkt_size < 0) {
 				// Check to see if we timed out 
 				if (errno == EAGAIN) {
 					// Has our time for this test expired?
-					float time_running = (1.0 * ((float)clock() - (float)start_clock)) / CLOCKS_PER_SEC;
+					float time_running = ((float)clock() - (float)start_clock) / CLOCKS_PER_SEC;
 					if (time_running > clock_timeout) {
-						print_stats(last_pkt_id, pkt_count);
+						print_stats(last_pkt_id, pkt_count, last_pkt_size);
 
 						pkt_count = 0;
 						last_pkt_id = 0;
 						start_clock = 0;
-						last_pkt_clock = 0;
+						//last_pkt_clock = 0;
 
 						break;
 					}
@@ -163,10 +212,14 @@ void run_server(short port)
 				continue;
 			}
 
-			pkt = (struct pkt_buffer*)buffer;
-			last_pkt_clock = clock();
-			last_pkt_id = pkt->id;
-			++pkt_count;
+			pkt = (struct pkt_buffer*)inner_buffer;
+			// Only count this packet if the hashes match
+			if (current_hash == pkt->hash) {
+				//last_pkt_clock = clock();
+				last_pkt_id = pkt->id;
+				last_pkt_size = pkt_size;
+				++pkt_count;
+			}
 		}
 	}
 
@@ -177,6 +230,8 @@ void run_server(short port)
 
 /**
  * Print the help document
+ *
+ * \param[in] progname			Name of the program, for the examples
  */
 void print_help(char *progname)
 {
@@ -198,20 +253,29 @@ void print_help(char *progname)
 		   progname, progname, progname, progname);
 }
 
+/**
+ * Entry point for the program
+ *
+ * \param[in] argc		Number of command line arguments
+ * \param[in] argv		Array of command line arguments
+ */
 int main(int argc, char **argv)
 {
 	int opt;
-	short port = DEFAULT_PORT;			// UDP port number
-	short type = TYPE_UNKNOWN;
-	short size = DEFAULT_SIZE;
+	int16_t port = DEFAULT_PORT;			// UDP port number
+	int16_t type = TYPE_UNKNOWN;
+	uint16_t size = DEFAULT_SIZE;
 	uint16_t timeout = DEFAULT_TIMEOUT;	
 	char *ip_addr;
 
 	while ((opt = getopt(argc, argv, "hb:c:p:st:")) != -1) {
 		switch (opt) {
-		case 'b':
-			size = atoi(optarg);
+		case 'b': {
+			long int tmp = strtol(optarg, NULL, BASE_10);
+			size = (tmp >= UINT16_MAX) ? UINT16_MAX : (uint16_t)tmp;
+			printf("%d\n", size);
 			break;
+		}
 		case 'c':
 			type = TYPE_CLIENT;
 			ip_addr = optarg;
